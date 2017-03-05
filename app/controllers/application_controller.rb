16daa6953e246455f3adec70b1ed3034b332e2fc
TheLucasMoore <lucasedwardmoore@gmail.com>
2017-03-05 15:36:29 -0700

    rollback controller authorization for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Basic::ControllerMethods
   include ActionController::HttpAuthentication::Token::ControllerMethods
-  acts_as_token_authentication_handler_for User
+  # acts_as_token_authentication_handler_for User
 end
